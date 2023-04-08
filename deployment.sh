79f4ba24bee47e48a5c83e03aad0551cfbc59949
Voi Bé Nhỏ <hymrdjnh199x9@gmail.com>
2023-04-08 19:34:23 +0700

    update deployment.sh file

--- deployment.sh
+++ deployment.sh
@@ -16,11 +16,7 @@ kubectl apply -f reverse-proxy-service.yaml
 
 kubectl expose deployment frontend --type=LoadBalancer --name=publicfrontend
 kubectl expose deployment reverseproxy --type=LoadBalancer --name=publicreverseproxy  
+# Auto scale
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-# Nếu update cluster
-# aws eks --region us-east-1 update-kubeconfig --name clusterProject3
-#kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
-# kubectl get ConfigMap/env-config -oyaml
-kubectl edit hpa   kubectl delete hpa backend-user  kubectl delete hpa backend-feed kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
+kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
 
-docker run --rm --publish 8080:8080 -v $HOME/.aws:/root/.aws --env POSTGRESS_HOST=$POSTGRESS_HOST --env POSTGRESS_USERNAME=$POSTGRESS_USERNAME --env POSTGRESS_PASSWORD=$POSTGRESS_PASSWORD --env POSTGRESS_DATABASE=$POSTGRESS_DATABASE --env AWS_REGION=$AWS_REGION --env AWS_PROFILE=$AWS_PROFILE --env AWS_BUCKET=$AWS_BUCKET --env JWT_SECRET=$JWT_SECRET --name cd0354-monolith-to-microservices-project_backend-feed_1
